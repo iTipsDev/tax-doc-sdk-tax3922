308064c14bf3ad93fd6442bae88615f5719a296f
Bruce V Wilcox <brucevwilcox@itips.info>
2025-11-20 14:54:38 -0700

    2025-11-20 14:54

--- src/main/java/org/taxdataexchange/usage/tax3922/Tax3922DocumentGeneratorAbbreviated.java
+++ src/main/java/org/taxdataexchange/usage/tax3922/Tax3922DocumentGeneratorAbbreviated.java
@@ -2,14 +2,13 @@ package org.taxdataexchange.usage.tax3922;
 
 import irs.fdxtax641.jsonserializers.Tax3922Serializer;
 import irs.fdxtax641.map2objects.Map2Tax3922;
-import org.taxdataexchange.core.pdfbuilders.Tax3922PdfBuilder;
-import org.taxdataexchange.core.utils.*;
-import org.taxdataexchange.core.csv.GenericCsvMapReader;
-
 import org.apache.commons.lang3.time.StopWatch;
-
-import org.openapitools.client.model.*;
-
+import org.openapitools.client.model.Tax3922;
+import org.taxdataexchange.core.csv.GenericCsvMapReader;
+import org.taxdataexchange.core.pdfbuilders.Tax3922PdfBuilder;
+import org.taxdataexchange.core.utils.FileUtils;
+import org.taxdataexchange.core.utils.Pdf2PngConverter;
+import org.taxdataexchange.core.utils.PdfWatermarker;
 import returnagnosticutils.BytesToFile;
 import returnagnosticutils.Jsonizer;
 import returnagnosticutils.StringToFile;
@@ -19,7 +18,7 @@ import java.util.Map;
 
 // Read CSV file for one company and generate 1099-MISC PDFs
 
-public class Tax3922DocumentGenerator {
+public class Tax3922DocumentGeneratorAbbreviated {
 
     public static final String INPUT_DIRECTORY = "input";
 
@@ -122,7 +121,6 @@ public class Tax3922DocumentGenerator {
         String companyName
     ) {
 
-        GenericCsvMapReader csvReader = new GenericCsvMapReader( );
 
         // CSV file
         String csvFileName  = "Tax3922.csv";
@@ -130,45 +128,58 @@ public class Tax3922DocumentGenerator {
         // CSV dir
         String csvDirName = String.format( "%s/%s", INPUT_DIRECTORY, companyName );
 
-        // Read CSV content
-        String csvContent = FileUtils.readDirFile(
-            csvDirName,
-            csvFileName
-        );
-
-        // Convert to list of maps
-        List<Map<String, String>> rows = csvReader.readStringWithCsvMapReader(
-            csvContent
-        );
 
         // Save
-        String outputDirName = String.format( "%s/%s", OUTPUT_DIRECTORY, companyName );
-        String outputFileName = "Tax3922.rows.json";
-        String asJson = Jsonizer.toJson( rows );
-        StringToFile.writeToDirFile( asJson, outputDirName, outputFileName );
+//        String outputDirName = String.format( "%s/%s", OUTPUT_DIRECTORY, companyName );
+//        String outputFileName = "Tax3922.rows.json";
+//        String asJson = Jsonizer.toJson( rows );
+//        StringToFile.writeToDirFile( asJson, outputDirName, outputFileName );
 
-        // Process each row
-        int rowNumber = 0;
-        for ( Map<String, String> row : rows ) {
-
-            rowNumber++;
-            processOneRow( companyName, rowNumber, row );
-
-        }
 
     }
 
     public static void main(String[] args) {
 
-        System.out.println( "Tax3922DocumentGenerator Begin" );
-
-        String companyName = "company1";
-
-        Tax3922DocumentGenerator generator = new Tax3922DocumentGenerator( );
-
-        generator.processCsvForCompany( companyName );
-
-        System.out.println( "Tax3922DocumentGenerator Done" );
+//        System.out.println( "Tax3922DocumentGenerator Begin" );
+//
+//        String companyName = "company1";
+//
+//        StringBuilder stringBuilder = new StringBuilder();
+//        StopWatch stopWatch = new StopWatch( );
+//        stopWatch.start( );
+//
+//        String csvContent = "";
+//
+//        // Read CSV content
+//        csvContent = FileUtils.readDirFile(
+//            csvDirName,
+//            csvFileName
+//        );
+//
+//        // Convert to list of maps
+//        GenericCsvMapReader csvReader = new GenericCsvMapReader( );
+//        List<Map<String, String>> rows = csvReader.readStringWithCsvMapReader(
+//            csvContent
+//        );
+//
+//        // Process row
+//        Map<String, String> row = rows.get( 0 );
+//
+//        // Convert to object
+//        Tax3922 taxObject = Map2Tax3922.generate( row );
+//
+//        // Generate PDF
+//        Tax3922PdfBuilder pdfBuilder = new Tax3922PdfBuilder( );
+//        pdfBuilder.buildBasicPdf( stringBuilder, stopWatch, taxObject );
+//        byte[] pdfBytes = pdfBuilder.getPrintPdfBytes1( );
+//
+//        // Save
+//        BytesToFile.writeToDirFile( pdfBytes,
+//            outputDir,
+//            fileName
+//        );
+//
+//        System.out.println( "Tax3922DocumentGenerator Done" );
 
     }
 
